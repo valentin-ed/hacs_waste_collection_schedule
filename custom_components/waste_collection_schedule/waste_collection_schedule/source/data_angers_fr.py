95ab27cf2242f71899676fcf3f5a8dc1578dcf61
Aguay-val <Aguay-val@users.noreply.github.com>
2025-01-14 14:13:32 +0100

    Create data_angers_fr.py
    Ref des api pour faire fonctionner les calls

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/data_angers_fr.py
@@ -0,0 +1,3 @@
+## https://data.angers.fr/api/explore/v2.1/catalog/datasets/secteurs-de-collecte-tri-et-plus/records?select=id_secteur&where=typvoie%3D%27ALLEE%27%20and%20libvoie%20like%20%22*cerisier*%22&limit=20&refine=lib_com%3A%22TRELAZE%22
+## https://data.angers.fr/api/explore/v2.1/catalog/datasets/calendrier-tri-et-plus/records?select=date_collecte&where=id_secteur%3D%2220160304152520700993%22&limit=20
+
